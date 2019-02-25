f7c212f6053605c04d49b3d91be3c279fab3b342
nirfuchs\fuchs <nirfuchs@hotmail.com>
2019-02-25 02:27:37 +0200

    Fix modal-cmp.js

--- js/cmps/book-details-cmp.js
+++ js/cmps/book-details-cmp.js
@@ -1,41 +1,20 @@
 import longText from './long-text-cmp.js';
+import modal from './modal-cmp.js';
 
 export default {
     props: ['book'],
     template: `
-       <!--<section v-if="book" class="book-details" v-bind:class="{'red-bg': book.listPrice.amount > 150, 'green-bg': book.listPrice.amount < 20}">-->
-        <div v-if="book" class="book-details modal" tabindex="-1" role="dialog" v-bind:class="{show : isShowModal}">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title">{{book.title}}</h5>
-                        <button v-on:click="onCloseModal" type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                        <!--<li v-for="(value, key) in book">
-                            {{ key }}:{{ value }}
-                        </li>-->
-                        <h3>{{pageCount}}</h3>
-                        <h3>{{publishedDate}}</h3>
-                        <h3 v-bind:class="{'red-bg': book.listPrice.amount > 150, 'green-bg': book.listPrice.amount < 20}">{{bookPrice}}</h3>
-                        <h3>{{onSale}}</h3>
-                        <long-text v-bind:txt="book.description"></long-text>
-                    </div>
-                    <div class="modal-footer">
-                        <button v-on:click="onCloseModal" type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!--</section>-->
+        <modal v-if="book" v-show="isShowModal" @close="isShowModal = false">
+            <h3 slot="header">{{book.title}}</h3>
+            <h3 slot="body">{{pageCount}}</h3>
+            <h3 slot="body">{{publishedDate}}</h3>
+            <h3 slot="body" v-bind:class="{'red-bg': book.listPrice.amount > 150, 'green-bg': book.listPrice.amount < 20}">{{bookPrice}}</h3>
+            <h3 slot="body">{{onSale}}</h3>
+        </modal>
     `,
     data() {
         return {
-            // bookPrice : this.book.listPrice.amount,
-            // isExpensive: true
-            isShowModal: true
+            isShowModal: false
         }
     },
     methods: {
@@ -79,6 +58,7 @@ export default {
     },
 
     components: {
-        longText
+        longText,
+        modal
     }
 }
---
+++ js/cmps/modal-cmp.js
@@ -0,0 +1,29 @@
+export default {
+    props: ['header', 'body', 'footer'],
+    template: `
+    <transition name="modal">
+        <div class="modal-mask">
+            <div class="modal-wrapper">
+                <div class="modal-container">
+                    <div class="modal-header">
+                        <slot name="header">
+                        </slot>
+                    </div>
+                    <div class="modal-body">
+                        <slot name="body">
+                        </slot>
+                    </div>
+                    <div class="modal-footer">
+                        <slot name="footer">
+                            <button class="modal-default-button" @click="$emit('close')">
+                                OK
+                            </button>
+                        </slot>
+                    </div>
+                </div>
+            </div>
+        </div>
+  </transition>
+    `,
+
+}
