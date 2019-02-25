7c111fed711125009153359de31326a8cb4c18a4
nirfuchs\fuchs <nirfuchs@hotmail.com>
2019-02-25 15:14:07 +0200

    Add the following pages to your app: home, about, book

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 import bookApp from './pages/book-app-cmp.js';
+import myRoutes from './routes.js';
+
+const myRouter = new VueRouter({routes: myRoutes});
+// console.log('myRoutes', myRoutes);
 
 window.vueApp = new Vue({
     el: '#app',
+    router: myRouter,
     components: {
         bookApp
     }
